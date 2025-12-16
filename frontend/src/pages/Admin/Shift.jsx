ac4673a16150678ded47fc0b3429877214e9cce3
Cristian <crisszz7@outlook.com>
2025-12-16 18:25:07 -0500

    Commit: Shifts page added

---
+++ frontend/src/pages/Admin/Shift.jsx
@@ -0,0 +1,24 @@
+
+export const Shift =() => {
+    return(
+        <div>
+            <h2> Turnos </h2>
+            <form action="">
+                <label htmlFor=""> Selecione su modulo: </label>
+                <select name="" id="">
+
+                    <option value=""></option>
+                    <option value=""></option> 
+                </select>
+                <label htmlFor=""> Selecione el servicio: </label>
+                <select name="" id=""> 
+                    {/* // To-Do: Fill options dynamically */}
+                </select>
+                <button type="submit">Generar </button>
+            </form>
+        </div>
+
+    )
+}
+
+export default Shift;
