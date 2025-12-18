92cc93ff43aff00970828814a0cc7a5ca1e3ac56
Cristian <crisszz7@outlook.com>
2025-12-18 08:18:41 -0500

    Commit: The table styles have been improved, now look like professional xd

--- frontend/src/pages/Admin/components/IU/TableComponent.jsx
+++ frontend/src/pages/Admin/components/IU/TableComponent.jsx
@@ -5,21 +5,21 @@ export const TableComponent = ({title, headers, data}) => {
     return(
         <div className="w-full h-max mt-5 p-3 text-white">
             <h2 className="font-bold w-full text-left text-3xl"> {title } <CreateButton /> </h2>
-            <table className="border border-white  mt-3 rounded-2xl min-w-11/12">
-                <thead>
-                    <tr className="border border-white bg-neutral-800">
+            <table className=" mt-5 rounded-2xl min-w-11/12">
+                <thead className="rounded-md">
+                    <tr className=" bg-neutral-900">
                         {headers.map((header, index) => (
-                            <th key={index} className="border border-white p-2"> {header} </th>
+                            <th key={index} className="text-left px-5 py-4"> {header} </th>
                         ))}
                         <th> Acciones </th>
                     </tr>
                 </thead>
-                <tbody className="bg-neutral-900">
+                <tbody className="bg-neutral-800">
                     {data.map((item, index) => (
                         <tr key={index} className="">
-                            <td className="border border-white text-center p-5"> {item.name} </td>
-                            <td className="border border-white text-center p-5">{ item.role ? item.role : item.word}</td>
-                            <td className="border border-white text-center">
+                            <td className="border-b border-t border-neutral-700  p-5"> {item.name} </td>
+                            <td className="border-b border-t border-neutral-700  p-5">{ item.role ? item.role : item.word}</td>
+                            <td className="border-b border-t border-neutral-700 text-center">
                                 <OptionButtons />
                             </td>
                         </tr>
